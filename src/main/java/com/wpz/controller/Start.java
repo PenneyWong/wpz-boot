dca96358c5a3337218ebc7b6dd5a83d89197f3d5
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-09 00:06:07 +0800

    新增一个方法

--- src/main/java/com/wpz/controller/Start.java
+++ src/main/java/com/wpz/controller/Start.java
@@ -1,5 +1,8 @@
 package com.wpz.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,17 @@ public class Start {
 	@RequestMapping("/hello")
 	public String start() {
 		return "Hello World";
+	}
+
+	@RequestMapping("/user")
+	public Map<String, Object> getMap() {
+
+		Map<String, Object> user = new HashMap<String, Object>();
+		
+		user.put("姓名", "王彭真");
+		user.put("年龄", "27");
+		user.put("性别", "男");
+		
+		return user;
 	}
 }
