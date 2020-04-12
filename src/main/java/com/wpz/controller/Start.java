af2f853edde01439113fc7e51ec9c1e68dcd675d
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-12 18:51:15 +0800

    修改

--- src/main/java/com/wpz/controller/Start.java
+++ src/main/java/com/wpz/controller/Start.java
@@ -3,36 +3,30 @@ package com.wpz.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class Start {
-	
-	@Autowired(required=false)
-	RedisTemplate redisTemplate;
-	
+
+	@ResponseBody
 	@RequestMapping("/hello")
-	public String start() {
-		redisTemplate.opsForValue().set("name", "WPZ");
-		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+	public String start () {
+		
 		return "Hello World";
 	}
-
+	
+	@ResponseBody
 	@RequestMapping("/user")
 	public Map<String, Object> getMap() {
 
 		Map<String, Object> user = new HashMap<String, Object>();
-		
+
 		user.put("姓名", "王彭真");
 		user.put("年龄", "27");
 		user.put("性别", "男");
-		
+
 		return user;
 	}
 }
--- src/main/java/com/wpz/dao/UserDao.java
+++ src/main/java/com/wpz/dao/UserDao.java
@@ -1,11 +1,10 @@
 package com.wpz.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.wpz.pojo.User;
-
-import io.lettuce.core.dynamic.annotation.Param;
 
 @Mapper
 public interface UserDao {
