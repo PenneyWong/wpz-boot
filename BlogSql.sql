b02c3e1fa69bf2cb985337bf917f44a30eb9fef8
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-19 19:12:28 +0800

    新增建表脚本

---
+++ BlogSql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `user` (
+  `id` varchar(100) DEFAULT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `age` varchar(5) DEFAULT NULL,
+  `sex` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表'
