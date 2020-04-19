01413578c53461427d1785b5699a32ec3fa04595
贾子墨 <jiazimo@sunline.cn>
2020-04-19 21:48:26 +0800

    测试

--- src/main/java/com/wpz/controller/UserController.java
+++ src/main/java/com/wpz/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.wpz.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wpz.pojo.User;
+import com.wpz.result.R;
 import com.wpz.service.UserDaoImpl;
 
 @RestController
@@ -16,5 +20,11 @@ public class UserController {
 	public String selectById() {
 
 		return userDaoImpl.selectById("2").toString();
+	}
+	
+	@RequestMapping("/selectUserList")
+	public R selectUserList() {
+		List<User> list = userDaoImpl.selectUserList();
+		return R.ok().data("items",list).message("测试列表");
 	}
 }
