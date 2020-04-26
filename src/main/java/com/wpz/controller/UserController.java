f2c3a29b9430a9783f71ab073cde9892da150ae1
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-27 00:13:16 +0800

    mybatis注解版

--- src/main/java/com/wpz/controller/UserController.java
+++ src/main/java/com/wpz/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.wpz.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.wpz.pojo.User;
-import com.wpz.result.R;
 import com.wpz.service.UserDaoImpl;
 
 @RestController
@@ -21,10 +20,9 @@ public class UserController {
 
 		return userDaoImpl.selectById("2").toString();
 	}
-	
+
 	@RequestMapping("/selectUserList")
-	public R selectUserList() {
-		List<User> list = userDaoImpl.selectUserList();
-		return R.ok().data("items",list).message("测试列表");
+	public List<Map> selectUserList() {
+		return userDaoImpl.selectUserList();
 	}
 }
--- src/main/java/com/wpz/dao/UserDao.java
+++ src/main/java/com/wpz/dao/UserDao.java
@@ -1,20 +1,20 @@
 package com.wpz.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import com.wpz.pojo.User;
 
 @Mapper
 public interface UserDao {
 
-	@Select("select * from user where id = #{id}")
+//	@Select("select * from user where id = #{id}")
 	public User selectById(@Param("id") String id);
 	
-	@Select("select * from user")
-	public List<User> selectUserList();
+//	@Select("select * from user")
+	public List<Map> selectUserList();
 
 }
