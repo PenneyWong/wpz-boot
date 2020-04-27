818f23d4a30030f2c4495696d54e16e4f8395206
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-27 21:59:12 +0800

    日志配置文件修改

--- src/main/java/com/wpz/controller/UserController.java
+++ src/main/java/com/wpz/controller/UserController.java
@@ -3,6 +3,8 @@ package com.wpz.controller;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import com.wpz.service.UserDaoImpl;
 
 @RestController
 public class UserController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserDaoImpl userDaoImpl;
