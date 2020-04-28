3c5d8545daebb0c515d9beb6afb605681be4c710
wangpengzhen <wangpengzhen1@sunline.cn>
2020-04-29 00:04:39 +0800

    添加kafka依赖

--- src/test/java/com/wpz/BlogApplicationTests.java
+++ src/test/java/com/wpz/BlogApplicationTests.java
@@ -1,21 +1,14 @@
 package com.wpz;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.wpz.pojo.Pig;
 
 @SpringBootTest
 class BlogApplicationTests {
 
-	@Autowired
-	private Pig pig;
-
 	@Test
 	void contextLoads() {
 
-		System.out.println(pig);
 	}
 
 }
