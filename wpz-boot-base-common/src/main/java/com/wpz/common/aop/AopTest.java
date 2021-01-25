1fab4732f904bd10f81efe3173ca13907ab4983f
wangpengzhen <pengzhenwang.com>
2021-01-25 10:55:16 +0800

    AOP测试

--- wpz-boot-base-common/src/main/java/com/wpz/common/aop/AopTest.java
+++ wpz-boot-base-common/src/main/java/com/wpz/common/aop/AopTest.java
@@ -1,7 +1,10 @@
 package com.wpz.common.aop;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -9,6 +12,23 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AopTest {
 
-    public void test() {
+    @Pointcut("execution(public * com.wpz.common.aop.Man.buy(..)) && args(price) &&bean(girl))")
+    public void setAdvice(String price) {
+    }
+
+    @Around("setAdvice(price)")
+    public String aroundAdvice(ProceedingJoinPoint pjp, String price) {
+
+        try {
+            pjp.proceed();
+            if (Double.parseDouble(price) > 60) {
+                System.out.println("女孩买衣服超过了60元，赠送一双袜子");
+                return "衣服和袜子";
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        return "衣服";
     }
 }
--- wpz-boot-base-common/src/test/java/com/wpz/common/test/AopTest.java
+++ wpz-boot-base-common/src/test/java/com/wpz/common/test/AopTest.java
@@ -1,16 +1,18 @@
 package com.wpz.common.test;
 
+import com.wpz.common.aop.Boy;
+import com.wpz.common.aop.Girl;
+import javafx.application.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = Application.class)
 public class AopTest {
 
     @Test
-    public void test(){
-
+    public void test() {
     }
 }
