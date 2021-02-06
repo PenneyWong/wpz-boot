b0803b6383591442f847477855bdb495d2dbdca2
wangpengzhen <pengzhenwang.com>
2021-02-06 15:22:16 +0800

    修改import

--- wpz-boot-base-system/src/main/java/com/wpz/aop/AopController.java
+++ wpz-boot-base-system/src/main/java/com/wpz/aop/AopController.java
@@ -1,6 +1,10 @@
 package com.wpz.aop;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aop")
